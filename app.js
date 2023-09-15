594ce4cb314d8994bb4e2aa0275e47238f4cd533
rslepfso <rslepfso@gmail.com>
2023-09-15 15:00:52 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+
+const config = require("./utils/config");
+
+app.use(express.json());
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => console.log("Connected"))
+  .catch((error) => console.log(`Something went wrong`, error.message));
+
+app.use("/api/blogs", blogsRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
